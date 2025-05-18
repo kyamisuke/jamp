9edb998cefe263bc2ce4bcd2e6da74569f6cb0ed
kyamisuke <kyamikyami0301@gmail.com>
2025-05-18 13:06:30 +0900

    project commit

---
+++ jamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jamp
+//
+//  Created by 村上航輔 on 2025/05/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jamp/jampApp.swift
@@ -0,0 +1,17 @@
+//
+//  jampApp.swift
+//  jamp
+//
+//  Created by 村上航輔 on 2025/05/18.
+//
+
+import SwiftUI
+
+@main
+struct jampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
